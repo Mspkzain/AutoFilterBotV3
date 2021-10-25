b561216acb8cee7d8b54f3ba1dd2e01fbcc6914d
Mspkzain <90121225+Mspkzain@users.noreply.github.com>
2021-10-25 22:37:07 +0530

    Update Main.py

--- LuciferMoringstar_Robot/Filter/Main.py
+++ LuciferMoringstar_Robot/Filter/Main.py
@@ -277,7 +277,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         elif query.data == "help":
             buttons = [
                 [
-                    InlineKeyboardButton('🔝 Main Menu', url=f'https://telegram.dog/Flash_The_Movie_FinderBot?start')
+                    InlineKeyboardButton('🔄 Restart', url=f'https://telegram.dog/Flash_The_Movie_FinderBot?start')
                 ]
                 ]
             await query.message.edit(text=f"{HELP}", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
@@ -285,7 +285,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         elif query.data == "about":
             buttons = [
                 [
-                    InlineKeyboardButton('🔝 Main Menu', url=f'https://telegram.dog/Flash_The_Movie_FinderBot?start')
+                    InlineKeyboardButton('🔄 Restart', url=f'https://telegram.dog/Flash_The_Movie_FinderBot?start')
                 ]
                 ]
             await query.message.edit(text=f"{ABOUT}", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)

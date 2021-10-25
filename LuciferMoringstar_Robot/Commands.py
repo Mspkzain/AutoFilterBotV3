e0ec67b5a3d34addaf6f3d9ce4f63291b7171136
Mspkzain <90121225+Mspkzain@users.noreply.github.com>
2021-10-25 21:04:02 +0530

    Update Commands.py

--- LuciferMoringstar_Robot/Commands.py
+++ LuciferMoringstar_Robot/Commands.py
@@ -334,5 +334,113 @@ async def bot_info(bot, message):
     buttons = [
         [
             
-            InlineKeyboardButton('Movie Group', url=f'{TUTORIAL}')]]
+            InlineKeyboardButton('Movie Group', url=f'{TUTORIAL}')], [InlineKeyboardButton('🔙 Back', async def start(bot, message):
+    chat_id = message.from_user.id
+    if not await db.is_user_exist(chat_id):
+        data = await bot.get_me()
+        BOT_USERNAME = data.username
+        await db.add_user(chat_id)
+        await bot.send_message(
+            LOG_CHANNEL,
+            f"#NEWUSER: \n\nNew User [{message.from_user.first_name}](tg://user?id={message.from_user.id}) started @{BOT_USERNAME} !!",
+        )
+    usr_cmdall1 = message.text
+    if usr_cmdall1.startswith("/start pr0fess0r_99"):
+        if AUTH_CHANNEL:
+            invite_link = await bot.create_chat_invite_link(int(AUTH_CHANNEL))
+            try:
+                user = await bot.get_chat_member(int(AUTH_CHANNEL), message.from_user.id)
+                if user.status == "kicked":
+                    await bot.send_message(
+                        chat_id=message.from_user.id,
+                        text="Sorry Sir, You are Banned to use me.",
+                        parse_mode="markdown",
+                        disable_web_page_preview=True
+                    )
+                    return
+            except UserNotParticipant:
+                ident, file_id = message.text.split("_-_-_-_")
+                await bot.send_message(
+                    chat_id=message.from_user.id,
+                    text="**Please Join My Updates Channel to use this Bot!**",
+                    reply_markup=InlineKeyboardMarkup(
+                        [
+                            [
+                                InlineKeyboardButton("📢 Join Updates Channel 📢", url=invite_link.invite_link)
+                            ],
+                            [
+                                InlineKeyboardButton("🔂 Try Again", callback_data=f"checksub#{file_id}")
+                            ]
+                        ]
+                    ),
+                    parse_mode="markdown"
+                )
+                return
+            except Exception:
+                await bot.send_message(
+                    chat_id=message.from_user.id,
+                    text="Something went Wrong.",
+                    parse_mode="markdown",
+                    disable_web_page_preview=True
+                )
+                return
+        try:
+            ident, file_id = message.text.split("_-_-_-_")
+            filedetails = await get_file_details(file_id)
+            for files in filedetails:
+                title = files.file_name
+                size=files.file_size
+                f_caption=files.caption
+                if CUSTOM_FILE_CAPTION:
+                    try:
+                        f_caption=CUSTOM_FILE_CAPTION.format(file_name=title, file_size=size, file_caption=f_caption)
+                    except Exception as e:
+                        print(e)
+                        f_caption=f_caption
+                if f_caption is None:
+                    f_caption = f"{files.file_name}"
+                buttons = [
+                    [
+                        InlineKeyboardButton('🎬 New Movies 🎬', url=f'{TUTORIAL}')
+                    ]
+                    ]
+                await bot.send_cached_media(
+                    chat_id=message.from_user.id,
+                    file_id=file_id,
+                    caption=f_caption,
+                    reply_markup=InlineKeyboardMarkup(buttons)
+                    )
+        except Exception as err:
+            await message.reply_text(f"Something went wrong!\n\n**Error:** `{err}`")
+    elif len(message.command) > 1 and message.command[1] == 'subscribe':
+        invite_link = await bot.create_chat_invite_link(int(AUTH_CHANNEL))
+        await bot.send_message(
+            chat_id=message.from_user.id,
+            text="**Please Join My Updates Channel to use this Bot!**",
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton("📢 Join Updates Channel 📢", url=invite_link.invite_link)
+                    ]
+                ]
+            )
+        )
+    else:
+        await message.reply_text(
+            START_MSG,
+            parse_mode="Markdown",
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup(
+                [[
+                InlineKeyboardButton("Request Group", url=f'https://t.me/joinchat/sxRlIPJYQQo4ODU1')
+                ],[
+                InlineKeyboardButton("Request Group", switch_inline_query_current_chat='')
+                ],[
+                InlineKeyboardButton("More", callback_data="help"),
+                InlineKeyboardButton("About", callback_data="about")
+                ],[
+                InlineKeyboardButton("🔙 Back", url=f'https://t.me/NS_File')]]
+            )
+        )
+        StopPropagation)]]
     await message.reply(text=f"{ABOUT}", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
